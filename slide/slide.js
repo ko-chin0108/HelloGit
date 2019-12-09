e9178d0e1942c4d603068d6af7bc8d6efcec3686
ko-chin0108 <51989551+ko-chin0108@users.noreply.github.com>
2019-12-09 09:22:39 +0900

    new slide

--- slide/slide.js
+++ slide/slide.js
@@ -1,27 +1,27 @@
-let sliderImages = document.querySelectorAll('.slider');
+let sliderImages = document.querySelectorAll('.slide');
 let arrowRight = document.getElementById('arrow-right');
 let arrowLeft = document.getElementById('arrow-left');
 let current = 0;
 
 function startSlide(){
-    sliderImages[0].getElementsByClassName.display = 'block';
+    sliderImages[0].style.display = 'block';
 }
 
 arrowRight.addEventListener('click',function(){
-    sliderImages[current].getElementsByClassName.display = 'none';
+    sliderImages[current].style.display = 'none';
     if(current === sliderImages.length-1){
         current = -1;
     }
-    sliderImages[current + 1].getElementsByClassName.display = 'block';
+    sliderImages[current + 1].style.display = 'block';
     current++;
 })
 
 arrowLeft.addEventListener('click',function(){
-    sliderImages[current].getElementsByClassName.display = 'none';
+    sliderImages[current].style.display = 'none';
     if(current === 0){
         current = sliderImages.length;
     }
-    sliderImages[current - 1].getElementsByClassName.display = 'block';
+    sliderImages[current - 1].style.display = 'block';
     current--;
 })
 
